8ec45472cb99587ff18849cdb5fe8f132dca1aad
axil0w <159565163+axil0w@users.noreply.github.com>
2024-11-06 19:21:21 +0100

    Add files via upload

--- app/models/question.py
+++ app/models/question.py
@@ -1,1 +1,20 @@
+class Question(object): #Pour les questions (type qcm par def)
+    def __init__(self, question, proposition, answer, subject, category):
+        self.question = question #queston en elle meme
+        self.proposition = proposition #differentes propositions
+        self.answer = answer #reponse
+        self.subject = subject #sujet plus precis de la question
 
+class TextQuestion(object): #Pour les questions (type qcm par def)
+    def __init__(self, question, proposition, answer, subject, category):
+        self.question = question #queston en elle meme
+        self.proposition = proposition #differentes propositions
+        self.answer = answer #reponse
+        self.subject = subject #sujet plus precis de la question
+
+class LinkQuestion(object): #Pour les questions (type qcm par def)
+    def __init__(self, question, proposition, answer, subject, category):
+        self.question = question #queston en elle meme
+        self.proposition = proposition #differentes propositions
+        self.answer = answer #reponse
+        self.subject = subject #sujet plus precis de la question
